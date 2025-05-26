f4aaca3fc2e46f7dee720f83d8132b2852d19082
Benjamin Clauss <benjaminclauss@protonmail.com>
2025-05-26 16:51:56 -0400

    Add skeleton for camera identification

---
+++ speeddaemon/model.go
@@ -0,0 +1,33 @@
+package speeddaemon
+
+// A Camera represents a speed camera.
+//
+// Each camera is on a specific road, at a specific location, and has a specific speed limit.
+// Each camera provides this information when it connects to the server.
+// Cameras report each number plate that they observe, along with the timestamp that they observed it.
+// Timestamps are exactly the same as [Unix timestamps] (counting seconds since 1st of January 1970), except that they are unsigned.
+//
+// [Unix timestamps]: https://en.wikipedia.org/wiki/Unix_time
+type Camera struct {
+	Road  uint16
+	Mile  uint16
+	Limit uint16
+}
+
+// A TicketDispatcher is responsible for some number of roads.
+//
+// When the server finds that a car was detected at 2 points on the same road with an average speed in excess of the
+// speed limit (speed = distance / time), it will find the responsible ticket dispatcher and send it a ticket for the
+// offending car, so that the ticket dispatcher can perform the necessary legal rituals.
+type TicketDispatcher struct {
+}
+
+// A Road in the network is identified by a number from 0 to 65535.
+//
+// A single road has the same speed limit at every point on the road.
+// Positions on the roads are identified by the number of miles from the start of the road.
+// Remarkably, all speed cameras are positioned at exact integer numbers of miles from the start of the road.
+type Road struct{}
+
+// A Car has a specific number plate represented as an uppercase alphanumeric string.
+type Car struct{}
---
+++ speeddaemon/server.go
@@ -0,0 +1,101 @@
+package speeddaemon
+
+import (
+	"encoding/binary"
+	"fmt"
+	"log/slog"
+	"net"
+)
+
+// SpeedLimitEnforcementServer coordinates enforcement of average speed limits on the Freedom Island road network.
+//
+// Two types of clients are supported: cameras and ticket dispatchers.
+// Clients connect over TCP and speak a protocol using a binary format.
+//
+// When the client does something that this protocol specification declares "an error", the server must send the
+// client an appropriate Error message and immediately disconnect that client.
+type SpeedLimitEnforcementServer struct {
+}
+
+var IllegalMessageType = &ErrorMessage{Msg: "illegal message"}
+
+// Handle handles a client connection.
+func (s *SpeedLimitEnforcementServer) Handle(client net.Conn) error {
+	defer closeOrLog(client)
+
+	var t uint8
+	if err := binary.Read(client, binary.BigEndian, &t); err != nil {
+		return fmt.Errorf("read error: %w", err)
+	}
+	switch t {
+	case IAmCameraMessageType:
+		return s.handleCamera(client)
+	case IAmDispatcherMessageType:
+		return s.handleDispatcher(client)
+	default:
+		slog.Error("unexpected message type", "type", t)
+		return sendError(client, IllegalMessageType)
+	}
+}
+
+var AlreadyIdentifiedError = &ErrorMessage{Msg: "client has already identified itself"}
+
+func (s *SpeedLimitEnforcementServer) handleCamera(client net.Conn) error {
+	m, err := readIAmCameraMessage(client)
+	if err != nil {
+		return fmt.Errorf("error reading IAmCamera message: %w", err)
+	}
+
+	c := Camera{Road: m.Road, Mile: m.Mile, Limit: m.Limit}
+	slog.Info("camera registered", "road", c.Road, "mile", c.Mile, "limit", c.Limit)
+
+	// TODO: Handle camera.
+
+	for {
+		var t uint8
+		// TODO: Should we ever disconnect client?
+		if err := binary.Read(client, binary.BigEndian, &t); err != nil {
+			return fmt.Errorf("read error: %w", err)
+		}
+
+		switch t {
+		case PlateMessageType:
+			// TODO: Implement.
+			return nil
+		case WantHeartbeatMessageType:
+			// TODO: Implement.
+			//It is an error for a client to send multiple WantHeartbeat messages on a single connection.
+			return nil
+		case IAmCameraMessageType:
+			return sendError(client, AlreadyIdentifiedError)
+		case IAmDispatcherMessageType:
+			return sendError(client, AlreadyIdentifiedError)
+		default:
+			return sendError(client, IllegalMessageType)
+		}
+	}
+}
+
+func (s *SpeedLimitEnforcementServer) handleDispatcher(client net.Conn) error {
+	return nil
+}
+
+// sendError sends an ErrorMessage to the client.
+func sendError(client net.Conn, m *ErrorMessage) error {
+	bytes, err := m.MarshalBinary()
+	if err != nil {
+		return fmt.Errorf("marshal error: %w", err)
+	}
+	_, err = client.Write(bytes)
+	if err != nil {
+		return fmt.Errorf("write error: %w", err)
+	}
+	return nil
+}
+
+// TODO: Move to utility package reusable for other problems.
+func closeOrLog(conn net.Conn) {
+	if err := conn.Close(); err != nil {
+		slog.Error("error closing connection", "err", err, "remote_addr", conn.RemoteAddr())
+	}
+}
